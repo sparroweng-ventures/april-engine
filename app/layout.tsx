f89e69164fb84870753c14a8b948ebb763ff4b13
sparroweng-ventures <arman@sparrow.engineering>
2026-09-05 19:17:22 +0530

    Change title from 'Morphic' to 'April Engine'

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'April Engine'
 const description =
   'A fully open-source AI-powered answer engine with a generative UI.'
 
