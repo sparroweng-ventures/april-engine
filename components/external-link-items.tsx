9257f200229d5abf7572d7db811cf251deedc7dc
sparroweng-ventures <arman@sparrow.engineering>
2026-09-10 16:51:11 +0530

    Update external links in external-link-items.tsx
    Removed Discord and X links from external links and updated GitHub link.

--- components/external-link-items.tsx
+++ components/external-link-items.tsx
@@ -2,28 +2,14 @@
 
 import Link from 'next/link'
 
-import {
-  IconBrandDiscord as SiDiscord,
-  IconBrandGithub as SiGithub,
-  IconBrandX as SiX
-} from '@tabler/icons-react'
+import { IconBrandGithub as SiGithub } from '@tabler/icons-react'
 
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
 
 const externalLinks = [
   {
-    name: 'X',
-    href: 'https://x.com/morphic_ai',
-    icon: <SiX className="size-4" />
-  },
-  {
-    name: 'Discord',
-    href: 'https://discord.gg/zRxaseCuGq',
-    icon: <SiDiscord className="size-4" />
-  },
-  {
     name: 'GitHub',
-    href: 'https://git.new/morphic',
+    href: 'https://github.com/sparrow-engineering/april-engine',
     icon: <SiGithub className="size-4" />
   }
 ]
